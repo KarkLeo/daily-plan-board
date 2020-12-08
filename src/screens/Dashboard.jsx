3fd68281211605bb4f8d5acb401914f8976ad469
Vyacheslav <v.malykhin.ff@gmail.com>
2020-12-09 00:03:48 +0200

    fix Dashboard

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -1,19 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { logOut } from "../store/user/authReducer";
 import TodosGrid from "../components/Todos/TodosGrid/TodosGrid";
-import DragAndDrop from "../components/DragAndDrop/DragAndDrop";
 import Icon from "../components/Sprite/Icon";
 
 const Dashboard = ({ logOut }) => {
-  // let [arr, serArr] = useState([
-  //   { id: 1, order: 1, title: "one" },
-  //   { id: 2, order: 2, title: "two" },
-  //   { id: 3, order: 3, title: "thee" },
-  //   { id: 4, order: 4, title: "fore" },
-  //   { id: 5, order: 5, title: "five" },
-  // ]);
-
   let date = new Date();
   return (
     <div className="dashboard">
@@ -33,9 +24,6 @@ const Dashboard = ({ logOut }) => {
       <div>
         <TodosGrid />
       </div>
-      {/*<div>*/}
-      {/*  <DragAndDrop list={arr} setList={serArr} />*/}
-      {/*</div>*/}
     </div>
   );
 };
