b0b57d4b8c7960506fad4ad44e07006312645e09
Vyacheslav <v.malykhin.ff@gmail.com>
2020-12-09 00:37:36 +0200

    test env

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App({ screen, testLocalToken }) {
     testLocalToken();
   }, []);
 
+  console.log(process.env);
   const router = (screen) => {
     switch (screen) {
       case "login":
