6c8e4cd56007cf4bf0c7d82d1a4172808c359f88
Vyacheslav <v.malykhin.ff@gmail.com>
2020-12-16 12:05:02 +0200

    item title done

--- src/api/todos.js
+++ src/api/todos.js
@@ -30,6 +30,30 @@ export const getDateTodos = async (token, userId, date) => {
   }
 };
 
+export const getUncompletedTodos = async (token, userId) => {
+  let date = new Date();
+  const query = qs.stringify({
+    _where: [
+      { user: userId },
+      { date_lt: stringifyDate(date) },
+      { status: "todo" },
+    ],
+  });
+  try {
+    let res = await axios.get(
+      `${process.env.REACT_APP_API_URL}/todos?${query}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return res.data;
+  } catch (err) {
+    return err;
+  }
+};
+
 export const createTodayTodos = async (token, userId, newTodo) => {
   let date = new Date();
   try {
